005e9af56bf5a8912337e903252f02b29db936a9
dylsdyl <dylanportelacavalcante@gmail.com>
2024-12-06 23:17:45 -0300

    Implementa somente telas onboarding

---
+++ lib/ui/onboarding/model/onboarding_step.dart
@@ -0,0 +1,12 @@
+// lib/model/onboarding_step.dart
+class OnboardingStep {
+  final String title;
+  final String description;
+  final String imageAsset;
+
+  OnboardingStep({
+    required this.title,
+    required this.description,
+    required this.imageAsset,
+  });
+}

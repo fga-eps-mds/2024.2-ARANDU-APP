856029d655528bd2b217df432a5c8c1c2e93dac4
R1K4S <ricardohsr1@gmail.com>
2024-12-07 00:33:26 -0300

    <Fix>(#59):Adpatação para o Modelo MVVM

--- lib/ui/edit_profile/view/EditProfile.dart
+++ lib/ui/edit_profile/view/EditProfile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:aranduapp/ui/shared/ProfileCard.dart';
+//import 'package:aranduapp/ui/shared/ProfileCard.dart';
 import 'package:aranduapp/ui/shared/TextEmail.dart';
 import 'package:aranduapp/ui/shared/CustomTextField.dart';
 import 'package:aranduapp/ui/shared/TextPassword.dart';
@@ -15,193 +15,200 @@ class EditProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(80), // Altura reduzida da AppBar
-        child: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          flexibleSpace: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(25),
-                bottomRight: Radius.circular(25),
-              ),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(
+              255, 255, 255, 255), // Define o fundo transparente
+          elevation: 0, // Remove a sombra da AppBar
+          title: Center(
+            child: Text(
+              'Editar perfil',
             ),
-            child: Padding(
-              padding:
-                  const EdgeInsets.only(top: 40.0, left: 16.0, right: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Botão de voltar
-                  IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: const Icon(Icons.arrow_back, color: Colors.black),
-                  ),
-                  // Título
-                  const Text(
-                    'Editar Perfil',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  // Ícone de notificação
-                  IconButton(
-                    onPressed: () {
-                      // Ação para notificações
-                    },
-                    icon: const Icon(
-                      Icons.notifications_outlined,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.notifications), // Ícone de notificação
+              onPressed: () {
+                // Ação quando o ícone de notificação for pressionado
+              },
+            ),
+          ],
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {},
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _icon(context),
+            _nameAndLast(context),
+            _emailAndPassword(context),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              _deleteButton(context),
+              _saveButton(context),
+            ])
+          ],
+        ));
+  }
+
+  Widget _nameAndLast(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          CustomTextField(
+            label: ' Primeiro Nome',
+            placeholder: 'Stefani',
+            controller: firstNameController,
+          ),
+
+          // Campo Último Nome usando CustomTextFieldq
+          CustomTextField(
+            label: ' Último Nome',
+            placeholder: 'Silva',
+            controller: lastNameController,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _emailAndPassword(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          TextEmail(
+            padding: const EdgeInsets.symmetric(horizontal: 0),
+            controller: emailController,
+          ),
+          TextPassWord(
+            padding: const EdgeInsets.symmetric(horizontal: 0),
+            controller: passwordController,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _saveButton(BuildContext context) {
+    return SingleChildScrollView(
+      child: ElevatedButton(
+        onPressed: () {
+          print('Primeiro Nome: ${firstNameController.text}');
+          print('Último Nome: ${lastNameController.text}');
+          print('E-mail: ${emailController.text}');
+        },
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size(200, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 0),
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+        ),
+        child: Ink(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(8, 145, 178, 1),
+                Color.fromRGBO(127, 219, 229, 1)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: const Text(
+              'Salvar',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
               ),
             ),
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
+    );
+  }
+
+  Widget _deleteButton(BuildContext context) {
+    return SingleChildScrollView(
+      child: ElevatedButton(
+        onPressed: () {
+          _showDeleteConfirmationDialog(context);
+        },
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size(200, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 0),
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+        ),
+        child: Ink(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(144, 11, 9, 1),
+                Color.fromRGBO(236, 34, 31, 1)
+              ],
+              begin: Alignment.bottomLeft,
+              end: Alignment.topLeft,
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: const Text(
+              'Deletar Conta',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _icon(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+          left: 16.0), // Adiciona espaço da borda esquerda
+      child: Row(
+        children: [
+          // Ícone circular
+          CircleAvatar(
+            radius: 30, // Tamanho do círculo
+          ),
+          SizedBox(width: 16), // Espaço entre a imagem e o texto
+          // Texto com o nome e o cargo
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Ícone abaixo da AppBar
-              const SizedBox(height: 16),
-              // Card com informações do perfil
-              ProfileCard(
-                name: 'Stefani',
-                role: 'Estudante',
-                avatarUrl: 'https://via.placeholder.com/150', // URL do avatar
+              Text(
+                "Stefani",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              const SizedBox(height: 32),
-
-              // Campo Nome usando CustomTextField
-              CustomTextField(
-                label: 'Primeiro Nome',
-                placeholder: 'Stefani',
-                controller: firstNameController,
-              ),
-              const SizedBox(height: 16),
-
-              // Campo Último Nome usando CustomTextField
-              CustomTextField(
-                label: 'Último Nome',
-                placeholder: 'Silva',
-                controller: lastNameController,
-              ),
-              const SizedBox(height: 16),
-
-              // Campo E-mail usando TextEmail
-              TextEmail(
-                padding: const EdgeInsets.symmetric(horizontal: 0),
-                controller: emailController,
-              ),
-              const SizedBox(height: 32),
-
-              // Campo Password usando TextPassword
-              TextPassWord(
-                padding: const EdgeInsets.symmetric(horizontal: 0),
-                controller: passwordController,
-              ),
-              const SizedBox(height: 32),
-
-              // Botões lado a lado
-              Row(
-                children: [
-                  // Botão Deletar Conta
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        _showDeleteConfirmationDialog(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 0),
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                      ),
-                      child: Ink(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromRGBO(144, 11, 9, 1),
-                              Color.fromRGBO(236, 34, 31, 1)
-                            ],
-                            begin: Alignment.bottomLeft,
-                            end: Alignment.topLeft,
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Container(
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.symmetric(vertical: 16),
-                          child: const Text(
-                            'Deletar Conta',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 64),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        print('Primeiro Nome: ${firstNameController.text}');
-                        print('Último Nome: ${lastNameController.text}');
-                        print('E-mail: ${emailController.text}');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 0),
-                        backgroundColor: Colors.transparent,
-                        shadowColor: Colors.transparent,
-                      ),
-                      child: Ink(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromRGBO(8, 145, 178, 1),
-                              Color.fromRGBO(127, 219, 229, 1)
-                            ],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Container(
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.symmetric(vertical: 16),
-                          child: const Text(
-                            'Salvar',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+              Text(
+                "Estudante",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
@@ -226,9 +233,6 @@ class EditProfile extends StatelessWidget {
                 print('Conta deletada');
                 Navigator.of(context).pop();
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
               child: const Text('Deletar'),
             ),
           ],

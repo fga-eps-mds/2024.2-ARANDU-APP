23823a84cd28bb78c989bb1f0614a790a33c6c68
MarinaGaldi <231035722@aluno.unb.br>
2024-12-12 17:32:57 -0300

    fix(#60):Correção do erro na página de recurar conta
    Co-authored-by: zDrNz <danrodrigues1920@gmail.com>

--- lib/ui/recover_account/view/RecoverAccount.dart
+++ lib/ui/recover_account/view/RecoverAccount.dart
@@ -9,55 +9,57 @@ class RecoverAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ChangeNotifierProvider(
-      create: (context) => RecoverAccountViewModel(context),
-      child: page(context),
-    ));
-  }
-
-  Widget page(BuildContext context) {
-    RecoverAccountViewModel viewModel =
-        Provider.of<RecoverAccountViewModel>(context);
-
-    return SingleChildScrollView(
-      child: Center(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 80),
-            const TitleSlogan(),
-            const Padding(
-              padding: EdgeInsets.only(top: 70, left: 20, right: 20),
-              child: Text(
-                'Coloque o seu e-mail no campo abaixo e clique em enviar. Logo em seguida, você receberá no seu e-mail as instruções para trocar a sua senha.',
-              ),
-            ),
-            Form(
-                key: viewModel.formKey,
-                child: Column(children: <Widget>[
-                  TextEmail(
-                      padding:
-                         const EdgeInsets.only(top: 24, left: 20, right: 20),
-                      controller: viewModel.emailController),
-                ])),
-            Padding(
-              padding: const EdgeInsets.only(top: 80),
-              child: SizedBox(
-                width: 291,
-                height: 64,
-                child: ElevatedButton(
-                    onPressed: () {}, child: const Text('Enviar')),
-              ),
-            ),
-            TextAndLink(
-              text: 'Já tem uma conta?',
-              link: ' Click aqui',
-              action: () {
-                Navigator.of(context).pop();
-              },
-            )
-          ],
-        ),
-      ),
+      body: ChangeNotifierProvider(
+          create: (context) => RecoverAccountViewModel(context),
+          builder: (context, child) {
+            return page(context);
+          }),
     );
   }
+}
+
+Widget page(BuildContext context) {
+  RecoverAccountViewModel viewModel =
+      Provider.of<RecoverAccountViewModel>(context);
+
+  return SingleChildScrollView(
+    child: Center(
+      child: Column(
+        children: <Widget>[
+          const SizedBox(height: 80),
+          const TitleSlogan(),
+          const Padding(
+            padding: EdgeInsets.only(top: 70, left: 20, right: 20),
+            child: Text(
+              'Coloque o seu e-mail no campo abaixo e clique em enviar. Logo em seguida, você receberá no seu e-mail as instruções para trocar a sua senha.',
+            ),
+          ),
+          Form(
+              key: viewModel.formKey,
+              child: Column(children: <Widget>[
+                TextEmail(
+                    padding:
+                        const EdgeInsets.only(top: 24, left: 20, right: 20),
+                    controller: viewModel.emailController),
+              ])),
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: SizedBox(
+              width: 291,
+              height: 64,
+              child:
+                  ElevatedButton(onPressed: () {}, child: const Text('Enviar')),
+            ),
+          ),
+          TextAndLink(
+            text: 'Já tem uma conta?',
+            link: ' Click aqui',
+            action: () {
+              Navigator.of(context).pop();
+            },
+          )
+        ],
+      ),
+    ),
+  );
 }

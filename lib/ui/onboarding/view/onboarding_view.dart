ed85187427aa07161ef085eec324cef6729af8ab
dylsdyl <dylanportelacavalcante@gmail.com>
2024-12-16 00:30:57 -0300

    fix(#58) Renomeia arquivos para evitar merge conflict

--- lib/ui/onboarding/view/onboarding_view.dart
+++ lib/ui/onboarding/view/onboarding_view.dart
@@ -11,19 +11,19 @@ class OnboardingView extends StatelessWidget {
       'title': 'Bem-Vindo(a)!',
       'description':
           'Seja bem vindo ao Arandu!!! Sou um ambiente voltado para aprendizado de forma gratuita',
-      'imageAsset': 'assets/images/Component1.1.png',
+      'imageAsset': 'assets/images/Component1.png',
     },
     {
       'title': 'o que você encontra?',
       'description':
           'Aqui você encontrará livros interativos de diversas disciplinas com objetivo de entregar praticidade e qualidade no aprendizado.',
-      'imageAsset': 'assets/images/Component2.1.png',
+      'imageAsset': 'assets/images/Component2.png',
     },
     {
       'title': 'Como funciona?',
       'description':
           'Para acessar uma trilha basta se inscrever na disciplina de sua preferência e ter acesso aos materiais. \n Bons estudos !!!',
-      'imageAsset': 'assets/images/Component3.1.png',
+      'imageAsset': 'assets/images/Component3.png',
     },
   ];
 

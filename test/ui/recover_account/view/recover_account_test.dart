791288439f963eb56775d7dde499304d97f7c39b
EngDann <danilodemeloribeiro0@gmail.com>
2025-01-16 17:18:16 -0300

    feat(#119): adiciona GetIt ao teste de recover_account

--- test/ui/recover_account/view/recover_account_test.dart
+++ test/ui/recover_account/view/recover_account_test.dart
@@ -6,6 +6,7 @@ import 'package:aranduapp/ui/shared/request_button.dart';
 import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
@@ -19,7 +20,7 @@ void main() {
   late MockRecoverAccountViewModel mockViewModel;
   late Command0 mockCommand0;
 
-  setUp(() {
+  setUp(() async {
     mockViewModel = MockRecoverAccountViewModel();
     mockCommand0 = MockCommand0();
 
@@ -28,6 +29,9 @@ void main() {
 
     when(mockViewModel.formKey).thenReturn(GlobalKey<FormState>());
     when(mockViewModel.emailController).thenReturn(TextEditingController());
+
+    await GetIt.instance.reset();
+    GetIt.I.registerLazySingleton<RecoverAccountViewModel>(() => mockViewModel);
   });
 
   Widget createLoginScreen(MockRecoverAccountViewModel mockViewModel) {

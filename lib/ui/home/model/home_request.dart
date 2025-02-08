1d20826055d1ea7f10a04ac2ed0a0dff6160c500
EngDann <danilodemeloribeiro0@gmail.com>
2025-02-07 22:26:06 -0300

    fix(#71): Altera requisições http da home

--- lib/ui/home/model/home_request.dart
+++ lib/ui/home/model/home_request.dart
@@ -2,22 +2,22 @@ import 'dart:convert';
 
 class HomeRequest {
   final String name;
+  final String id;
+  final String description;
 
-  HomeRequest({
-    required this.name,
-  });
+  HomeRequest(
+      {required this.name, required this.id, required this.description});
 
   Map<String, dynamic> toJson() {
-    return <String, dynamic>{
-      'name': name,
-    };
+    return <String, dynamic>{'name': name, '_id': id};
   }
 
   factory HomeRequest.fromJsonString(String jsonString) {
     final json = jsonDecode(jsonString);
 
     return HomeRequest(
-      name: json['name']! as String,
-    );
+        name: json['name']! as String,
+        id: json['_id']! as String,
+        description: json["description"]! as String);
   }
 }
--- lib/ui/home/service/home_service.dart
+++ lib/ui/home/service/home_service.dart
@@ -1,26 +1,40 @@
 import 'package:aranduapp/core/log/log.dart';
 import 'package:aranduapp/core/network/studio_maker_api.dart';
-import 'package:aranduapp/ui/home/model/home_model.dart';
-import 'package:aranduapp/ui/home/model/home_request.dart';
+import 'package:aranduapp/ui/home/model/home_knowledge_request.dart';
 import 'package:dio/dio.dart';
 
 class HomeService {
-  Future<List<HomeModel>> getKnowledges(HomeRequest homeRequest) async {
-    Response response =
-        await StudioMakerApi.getInstance().get(path: '/knowledges');
+  Future<List<Map<String, dynamic>>?> getKnowledges(
+      HomeRequest homeRequest) async {
+    try {
+      String path = '/knowledges';
+      Response response = await StudioMakerApi.getInstance().get(path: path);
+      if (response.statusCode == 200 && response.data != null) {
+        return List<Map<String, dynamic>>.from(response.data);
+      } else {
+        Log.e('Erro ao buscar áreas de conhecimento:${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      Log.e('Erro na requisição $e');
+      return null;
+    }
+  }
 
-    List<dynamic> nameList = response.data as List<dynamic>;
-
-    Log.i(nameList);
-
-    var res = nameList.map((e) {
-      final Map<String, dynamic> nameMap = e as Map<String, dynamic>;
-
-      return HomeModel(
-        name: nameMap['name']! as String,
-      );
-    }).toList();
-
-    return res;
+  Future<List<Map<String, dynamic>>?> getSubjects(
+      String knowledgeId, String name) async {
+    try {
+      String path = '/knowledges/$knowledgeId';
+      Response response = await StudioMakerApi.getInstance().get(path: path);
+      if (response.statusCode == 200 && response.data != null) {
+        return List<Map<String, dynamic>>.from(response.data);
+      } else {
+        Log.e('Erro ao buscar as disciplinas de $name :${response.statusCode}');
+        return null;
+      }
+    } catch (e) {
+      Log.e('Erro na requisição $e');
+      return null;
+    }
   }
 }
